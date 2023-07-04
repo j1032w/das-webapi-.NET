f260e3d97bbaa52d61ce167796f320dc9634c2f2
J1032 <J1032@example.com>
2023-07-04 04:57:50 -0600

    Added database creating scripts

---
+++ database/dbo.uspResidentialPropertyFindById.sql
@@ -0,0 +1,33 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+
+CREATE PROC [dbo].[uspResidentialPropertyFindById] @Id INT
+AS
+SELECT
+  Id
+ ,BuildingAmenity
+ ,BuildingBathroomTotal
+ ,BuildingBedroom
+ ,BuildingSizeInterior
+ ,BuildingStoriesTotal
+ ,BuildingType
+ ,Distance
+ ,LandLandscapeFeature
+ ,LandSizeTotal
+ ,ListingBoundary
+ ,MlsNumber
+ ,PostalCode
+ ,PriceUnformattedValue
+ ,PropertyAmenityNearBy
+ ,PropertyOwnershipType
+ ,PropertyParkingSpaceTotal
+ ,PropertyParkingType
+ ,PropertyType
+ ,ProvinceName
+ ,PublicRemark
+ ,City
+ ,ListedTime
+ ,ModifiedTime
+FROM ResidentialProperty
+WHERE Id = @Id
+GO
